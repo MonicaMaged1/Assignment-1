9ee8814cae2b98316ad1cdd441d2b27c1ad837fc
MonicaMaged1 <32578735+MonicaMaged1@users.noreply.github.com>
2017-10-10 23:20:09 +0300

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -10,17 +10,29 @@ package sheet1_3;
  * @author Lenovo
  */
 public class Triangle implements Shape {
-    private int a;
-    private int b;
-    private int c;
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+    private int x3;
+    private int y3;
+    private double a = Math.sqrt((x1-x2)^2 + (y1-y2)^2);
+    private double b = Math.sqrt((x2-x3)^2 + (y2-y3)^2);
+    private double c = Math.sqrt((x1-x3)^2 + (y1-y3)^2);
 
-    public Triangle(int a, int b, int c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    public Triangle(int x1, int y1, int x2, int y2, int x3, int y3) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.x3 = x3;
+        this.y3 = y3;
     }
+
+    
     
     public double calculateArea(){
+        
         double s = (a+b+c)/2;
         return Math.sqrt(s*(s-a)*(s-b)*(s-c));
     }
